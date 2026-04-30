099afe7d72fcd669b756965e5a8d61f052012eb2
Jonathan Esquivel <jesqpa@gmail.com>
2026-04-30 17:05:35 -0600

    Correction in json handling exercises

--- Python Basico/Manejo de JSON/questions61.py
+++ Python Basico/Manejo de JSON/questions61.py
@@ -1,26 +1,22 @@
 import json
 
 def extract_json(path): 
-	json_text = ""
-	with open(path, 'r', encoding='utf-8' ) as file: 
-		for line in file.readlines(): 			
-			text = line.strip()	
-			json_text += text	
-	return json.loads(json_text)
+	with open(path, 'r', encoding='utf-8' ) as file: 		
+		return json.load(file)
 
 
 def print_pokemons(data, type_filter):
 	counter = 0
-	list = []
+	pokemon_list = []
 	for pokemon in data:		
 		if pokemon['type'][0].lower() == type_filter.lower():
-			list.append(pokemon['name']['english'])
+			pokemon_list.append(pokemon['name']['english'])
 			counter += 1			
 	if counter == 0:
 		print("No se encontraron Pokémon de ese tipo.")	
 	else:
 		print(f"Los Pokémon de tipo {type_filter} son:")
-		for name in list:
+		for name in pokemon_list:
 			print(name)
 
 def main():
